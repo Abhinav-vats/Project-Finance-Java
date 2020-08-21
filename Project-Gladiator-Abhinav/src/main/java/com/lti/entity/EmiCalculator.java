7a215e3b29508c4c093bd7266cd6be77b7f6ff27
Nupur Jain <Nupur.Jain@lntinfotech.com>
2020-08-22 00:03:21 +0100

    EMI CALCULATOR LOGIC

---
+++ Project-Gladiator-Abhinav/src/main/java/com/lti/entity/EmiCalculator.java
@@ -0,0 +1,27 @@
+package com.lti.entity;
+
+public class EmiCalculator {
+
+	
+	
+	
+		private int p;//amount
+		private float r;//rate
+		private int n;
+		public double emi;
+		public EmiCalculator(int p, float r, int n)//amount,rate,year 
+		{
+			super();
+			this.p = p;
+			this.r = r/1200;
+			this.n = n*12;
+		}
+		public double calcEmi() {
+			emi=(p*r*Math.pow(1+r,n))/(Math.pow(1+r,n)-1);
+			return emi;
+		}
+		
+		
+	}
+
+
